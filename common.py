1617560fb41bbc06b04559b679b34968a7aca960
Mateusz Czeladka <mszczap@team.mobile.de>
2017-10-11 09:09:59 +0200

    car makes

--- common.py
+++ common.py
@@ -1,8 +1,10 @@
 __all__ = ('CLASS_NAME')
 
 CLASS_NAME = [
-    'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
-    'Ford', 'Google', 'HP', 'Heineken', 'Intel', 'McDonalds', 'Mini', 'Nbc',
-    'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
-    'Texaco', 'Unicef', 'Vodafone', 'Yahoo'
+    'BMW',
+    'Citroen',
+    'Ferrari',
+    'Ford',
+    'Mini',
+    'Porsche'
 ]
